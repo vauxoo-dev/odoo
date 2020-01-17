5f6da391c0548c0b41d5555f0ac15d1f7dd60388
Humberto Arocha <hbto@vauxoo.com>
2020-01-17 15:28:24 -0600

    account: Better Wording at button_cancel() method
    When Unposting several Journal Entries with button_cancel() method there
    could be some JE that cannot be unposted but information provide gives
    no clue on which journal entry the UserError arises.
    
    Signed-off-by: MQT Patches <mqt@patches>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -381,7 +381,8 @@ class AccountMove(models.Model):
     def button_cancel(self):
         for move in self:
             if not move.journal_id.update_posted:
-                raise UserError(_('You cannot modify a posted entry of this journal.\nFirst you should set the journal to allow cancelling entries.'))
+                err_msg = _('Journal name (id): %s (%s)') % (move.journal_id.name, str(move.journal_id.id))
+                raise UserError(_('You cannot modify a posted entry of this journal.\nFirst you should set the journal to allow cancelling entries.\n%s.') % err_msg)
             # We remove all the analytics entries for this journal
             move.mapped('line_ids.analytic_line_ids').unlink()
         if self.ids:
