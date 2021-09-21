2cc8c4807f8e7e1dca1fda573f19e30af028d4b4
Fernanda Hernández <fernanda@vauxoo.com>
2021-09-21 17:27:35 +0000

    [REF] base: archive merged partners instead unlink

--- odoo/addons/base/wizard/base_partner_merge.py
+++ odoo/addons/base/wizard/base_partner_merge.py
@@ -327,7 +327,7 @@ class MergePartnerAutomatic(models.TransientModel):
         self._log_merge_operation(src_partners, dst_partner)
 
         # delete source partner, since they are merged
-        src_partners.unlink()
+        src_partners.write({src_partners._active_name: False})
 
     def _log_merge_operation(self, src_partners, dst_partner):
         _logger.info('(uid = %s) merged the partners %r with %s', self._uid, src_partners.ids, dst_partner.id)
